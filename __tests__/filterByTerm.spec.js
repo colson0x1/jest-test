adf2a17c23ee3d33f939c0232053dcdcbe6ef373
Colson <colsoncalifornication@gmail.com>
2023-07-13 07:33:13 +0545

    write a failing test

---
+++ __tests__/filterByTerm.spec.js
@@ -0,0 +1,14 @@
+describe('Filter function', () => {
+  // test stuff
+  test('it should filter by a search term', () => {
+    const input = [
+      { id: 1, url: 'https://www.url1.dev' },
+      { id: 2, url: 'https://www.url2.dev' },
+      { id: 3, url: 'https://www.link3.dev' },
+    ];
+
+    const output = [{ id: 3, url: 'https://www.link3.dev' }];
+
+    expect(filterByTerm(input, 'link')).toEqual(output);
+  });
+});
